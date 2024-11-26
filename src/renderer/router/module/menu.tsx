b554f84499d37e82894054d3927732063847d902
chenZhiLong <97491530@qq.com>
2024-11-26 14:46:05 +0800

    feat:添加菜单以及字体

---
+++ src/renderer/router/module/menu.tsx
@@ -0,0 +1,35 @@
+import HomePage from '@pages/homePage/index';
+import Todo from '@pages/menu/todo';
+import type { IconSetType } from '@src/common/constants/iconSet';
+
+export type IMenutTypeItem = {
+  path: string;
+  element: React.ReactNode;
+  meta: {
+    title: string;
+    icon: IconSetType;
+    className?: string;
+  };
+};
+
+const routes: IMenutTypeItem[] = [
+  {
+    path: '',
+    element: <HomePage />,
+    meta: {
+      title: '盒子',
+      icon: 'home',
+      className: 'w-[22px] h-[22px]',
+    },
+  },
+  {
+    path: 'todo',
+    element: <Todo />,
+    meta: {
+      title: '待办',
+      icon: 'todo',
+    },
+  },
+];
+
+export default routes;
