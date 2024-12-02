bf8938b675c98b4e373854b7e6fa6cd6dc76f625
陈智龙 <97491530@qq.com>
2024-12-03 01:07:04 +0800

    fix:调整登录流程

--- electron/telegramCore/index.ts
+++ electron/telegramCore/index.ts
@@ -1,5 +1,5 @@
 import { ipcMain, BrowserWindow, IpcMainEvent } from 'electron';
-import { TelegramClient, sessions } from 'telegram';
+import { TelegramClient, sessions, Api } from 'telegram';
 import { IpcKey } from '../ipc/ipcKey';
 import { TgErrorConst } from '../../common/const';
 const stringSession = new sessions.StringSession('');
@@ -14,26 +14,51 @@ const initTg = async () => {
   return client;
 };
 
-const loginTg = async (event: IpcMainEvent, params: { username: string; password?: string }) => {
+const sendBotQueryStatus = async () => {
+  try {
+    const result = await client.invoke(
+      new Api.messages.SendMessage({
+        peer: '@SpamBot',
+        message: '/start',
+        noWebpage: true,
+        noforwards: true,
+      }),
+    );
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const loginTg = async (event: IpcMainEvent, params: { username: string; password: string }) => {
   if (!client) client = await initTg();
-  await client.start({
-    phoneNumber: params.username,
-    phoneCode: async () => {
-      const result: string = await new Promise((resolve) => {
-        ipcMain.once(IpcKey.confirmPhoneCode, (_event: IpcMainEvent, code: string) => resolve(code));
-        const win = BrowserWindow.fromWebContents(event.sender);
-        win?.webContents.send(IpcKey.onConfirmPhoneCodeSend);
-      });
-      return result;
-    },
-    password: undefined,
-    onError: (error) => {
-      const message = TgErrorConst[error.message];
-      const win = BrowserWindow.fromWebContents(event.sender);
-      win?.webContents.send(IpcKey.onToastMessage, message, 'error');
-    },
-  });
-  client.session.save();
+
+  try {
+    // await client.start({
+    //   phoneNumber: params.username,
+    //   phoneCode: async () => {
+    //     const result: string = await new Promise((resolve) => {
+    //       ipcMain.once(IpcKey.confirmPhoneCode, (_event: IpcMainEvent, code: string) => resolve(code));
+    //       const win = BrowserWindow.fromWebContents(event.sender);
+    //       win?.webContents.send(IpcKey.onConfirmPhoneCodeSend);
+    //     });
+    //     return result;
+    //   },
+    //   password: params.password ? () => Promise.resolve(params.password) : undefined,
+    //   onError: (error) => {
+    //     const message = TgErrorConst[error.message];
+    //     const win = BrowserWindow.fromWebContents(event.sender);
+    //     win?.webContents.send(IpcKey.onToastMessage, message, 'error');
+    //   },
+    // });
+    const key = client.session.save();
+    console.log(key);
+    await client.connect();
+    await sendBotQueryStatus();
+  } catch (error) {
+    const win = BrowserWindow.fromWebContents(event.sender);
+    win?.webContents.send(IpcKey.onToastMessage, error, 'error');
+  }
 };
 
 export { initTg, loginTg };
