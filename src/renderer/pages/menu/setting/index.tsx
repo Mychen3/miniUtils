6f1533fe8c5f0c5d6fbc020b265684b24791a5d5
Mychen3 <97491530@qq.com>
2025-04-28 18:21:33 +0800

    fix:'增加待授权'

--- src/renderer/pages/menu/setting/index.tsx
+++ src/renderer/pages/menu/setting/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './css/index.module.scss';
+import { Input, Button } from '@nextui-org/react';
 
 const Setting = () => {
   const [setting, setSetting] = useState<any>({});
@@ -7,6 +8,14 @@ const Setting = () => {
     <div className={styles.container}>
       <div className={styles.title}>激活许可证</div>
       <div className={styles.titleTip}>使用您获取的密钥激活软件！</div>
+      <div className={styles.inputContainer}>
+        <Input type="password" variant="bordered"></Input>
+      </div>
+      <div className={styles.subBtn}>
+        <Button color="primary" className="w-[100px]">
+          激活
+        </Button>
+      </div>
     </div>
   );
 };
